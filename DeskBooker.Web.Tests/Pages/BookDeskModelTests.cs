5926abc2540e3e29faf9454c004a4d5b12d2faf0
Guilherme <guima_dev@hotmail.com>
2021-07-20 18:44:26 -0300

    .

---
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -0,0 +1,33 @@
+﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using DeskBooker.Web.Pages;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace DeskBooker.Web.Tests.Pages
+{
+    public class BookDeskModelTests
+    {
+        [Fact]
+        public void ShouldCallBookDeskMethodOfProcessor()
+        {
+            //Arange
+            var processorMock = new Mock<IDeskBookingRequestProcessor>();
+
+            var bookDeskModel = new BookDeskModel(processorMock.Object)
+            {
+                DeskBookingRequest = new DeskBookingRequest()
+            };
+            //Act
+            bookDeskModel.OnPost();
+
+            //Assert
+
+        }
+    }
+}
